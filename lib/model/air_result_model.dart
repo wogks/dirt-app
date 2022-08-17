61e5330ef668b49c933569216cc60e6ba38769e3
wogks <wogks27@naver.com>
2022-08-18 02:14:09 +0900

    Add: model 작성

---
+++ lib/model/air_result_model.dart
@@ -0,0 +1,7 @@
+class AirResultModel {
+  String aqius;
+  String aqicn;
+  String ws;
+
+  AirResultModel({required this.aqius, required this.aqicn, required this.ws});
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,22 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dirtapp/model/air_result_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:http/http.dart' as http;
 import 'package:dirtapp/main.dart';
+import 'dart:convert';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('http 통신 테스트', () async {
+    Uri url = Uri.parse('http://api.airvisual.com/v2/nearest_city?key=dbf8360b-ce49-4c12-a141-a2c43a0ca016');
+    http.Response response = await http.get(url);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(response.statusCode, 200);
+   
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    AirResult result = AirResult.fromJson(jsonDecode(response.body));
+    expect(result.status, 'success');
   });
 }
